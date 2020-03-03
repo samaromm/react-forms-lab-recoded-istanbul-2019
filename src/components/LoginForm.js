baf46ac88faf09a927acd422ad9ead7144fbd7a9
Samar Omar <samaromm98@gmail.com>
2020-03-03 21:06:56 +0000

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,40 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username:"",
+      password:""
+    };
+  }
+  
+  handleChange=event=>{
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  
+  handleSubmit=event=>{
+    event.preventDefault()
+    let formData= {
+      username: this.state.username,
+      password: this.state.password}
+    if(formData.username!=''&&formData.password!='')
+      this.props.handleLogin(formData)  
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} onChange={this.handleChange} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" value={this.state.password} onChange={this.handleChange}/>
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value:""
+    };
+  }
+  
+  handleChange=event=>{
+    this.setState({
+      value:event.target.value
+    })
+    
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.value} onChange={this.handleChange}/>
+        <h2>remainder chars: {this.props.maxChars - this.state.value.length}</h2>
       </div>
     );
   }
