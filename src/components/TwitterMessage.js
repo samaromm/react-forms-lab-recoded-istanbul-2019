b9f6f41995ed7ac128081291109c99d08ca672b0
Samar Omar <samaromm98@gmail.com>
2020-03-06 01:06:29 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -5,14 +5,26 @@ class TwitterMessage extends React.Component {
     super();
 
     this.state = {
+<<<<<<< HEAD
       value:""
+=======
+      message:"",
+      restChar:this.props.maxChars 
+>>>>>>> 00d785b5b8f4646bd64e4ebaa6eab92f3bd76374
     };
   }
   
   handleChange=event=>{
+<<<<<<< HEAD
     this.setState({
       value:event.target.value
     })
+=======
+    this.setState(){
+      message:event.target.value,
+      restChar:this.props.maxChars-this.state.message.length
+    }
+>>>>>>> 00d785b5b8f4646bd64e4ebaa6eab92f3bd76374
     
   }
 
@@ -20,8 +32,12 @@ class TwitterMessage extends React.Component {
     return (
       <div>
         <strong>Your message:</strong>
+<<<<<<< HEAD
         <input type="text" name="message" id="message" value={this.state.value} onChange={this.handleChange}/>
         <h2>remainder chars: {this.props.maxChars - this.state.value.length}</h2>
+=======
+        <input type="text" name="message" id="message" value={this.state.message} onChange={this.handleChange(event)}/>
+>>>>>>> 00d785b5b8f4646bd64e4ebaa6eab92f3bd76374
       </div>
     );
   }
